91f12e14d230ac0f97c602983f7fb6f47011548f
Dmitry Tsirulnikov <dmitry.tsirulnikov@gmail.com>
2018-12-13 00:37:19 +0200

    startsh modified. added deta back

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-(tiddlywiki ~/wiki --init server || true )&& tiddlywiki ~/wiki --server 8080 $:/core/save/all text/plain text/html  ${USERNAME:-user} ${PASSWORD:-'wiki'} 0.0.0.0
+(tiddlywiki /data/wiki --init server || true )&& tiddlywiki /data/wiki --server 8080 $:/core/save/all text/plain text/html  ${USERNAME:-user} ${PASSWORD:-'wiki'} 0.0.0.0
