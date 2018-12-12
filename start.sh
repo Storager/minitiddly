7468f6435305f1c6c813f141d4ec419113b52ebb
Dmitry Tsirulnikov <dmitry.tsirulnikov@gmail.com>
2018-12-13 00:08:59 +0200

    chenged working directory

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-(tiddlywiki /opt/wiki --init server || true )&& tiddlywiki /opt/wiki --server 8080 $:/core/save/all text/plain text/html  ${USERNAME:-user} ${PASSWORD:-'wiki'} 0.0.0.0
+(tiddlywiki ~/wiki --init server || true )&& tiddlywiki ~/wiki --server 8080 $:/core/save/all text/plain text/html  ${USERNAME:-user} ${PASSWORD:-'wiki'} 0.0.0.0
